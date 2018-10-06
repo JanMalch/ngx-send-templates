c8c26ddb799042e2c1acf3f2cf52296e39481ebf
JanMalch <janmalch@gmail.com>
2018-10-06 17:04:08 +0200

    Refactored directive inputs Fixed demo and README accordingly

--- src/app/examples/stepper.component.ts
+++ src/app/examples/stepper.component.ts
@@ -5,9 +5,9 @@ import {BehaviorSubject} from 'rxjs';
 @Component({
   selector: 'app-stepper',
   template: `
-    <h3 *sendTemplate="'stepper'; sendOn: stepper$; pipe: onStep(1)">Step 1</h3>
-    <h3 *sendTemplate="'stepper'; sendOn: stepper$; pipe: onStep(2)">Step 2</h3>
-    <h3 *sendTemplate="'stepper'; sendOn: stepper$; pipe: onStep(3)">Step 3</h3>
+    <h3 *sendTemplate="'stepper' on stepper$ do onStep(1)">Step 1</h3>
+    <h3 *sendTemplate="'stepper' on stepper$ do onStep(2)">Step 2</h3>
+    <h3 *sendTemplate="'stepper' on stepper$ do onStep(3)">Step 3</h3>
 
     <h3 *receiveTemplate="'stepper'">Click the button start!</h3>
     <button (click)="nextStep()">Next Step</button>
